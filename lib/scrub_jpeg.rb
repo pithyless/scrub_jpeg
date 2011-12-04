ea0bfba869c8e2b0e65a18c38ae86668e2a432f5
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-12-04 15:23:33 +0100

    add version 0.0.1 of scrub_jpeg and view_jpeg_info

--- lib/scrub_jpeg.rb
+++ lib/scrub_jpeg.rb
@@ -1,5 +1,55 @@
 require "scrub_jpeg/version"
+require 'exiv2'
 
 module ScrubJpeg
-  # Your code goes here...
+  class Scrubber
+    def initialize(filename)
+      info "Scrubbing... #{filename}"
+      @image = Exiv2::ImageFactory.open(filename)
+      @image.read_metadata
+    rescue Exiv2::BasicError
+      warn "Cannot open file: #{filename}"
+    end
+
+    def scrub!
+      delete_all_iptc_tags
+      delete_all_exif_tags
+      delete_all_xmp_tags
+      @image.write_metadata
+    end
+
+    private
+
+    def iptc_tags
+      @image.iptc_data.map(&:first)
+    end
+
+    def exif_tags
+      @image.exif_data.map(&:first)
+    end
+
+    def xmp_tags
+      @image.xmp_data.map(&:first)
+    end
+
+    def delete_all_iptc_tags
+      iptc_tags.each { |x| @image.iptc_data.delete_all(x) }
+    end
+
+    def delete_all_exif_tags
+      exif_tags.each { |x| @image.exif_data.delete_all(x) }
+    end
+
+    def delete_all_xmp_tags
+      xmp_tags.each { |x| @image.xmp_data.delete_all(x) }
+    end
+
+    def info(msg)
+      puts "#{msg}"
+    end
+
+    def warn(msg)
+      puts "[WARNING] #{msg}"
+    end
+  end
 end
--- scrub_jpeg.gemspec
+++ scrub_jpeg.gemspec
@@ -20,5 +20,5 @@ Gem::Specification.new do |s|
 
   # specify any dependencies here; for example:
   # s.add_development_dependency "rspec"
-  # s.add_runtime_dependency "rest-client"
+  s.add_runtime_dependency 'exiv2', '0.0.6'
 end
